f5d7aeb497e1562c5c04d7458803db80ecaceec7
Zahraoui Saad <saadxzhr1@gmail.com>
2026-04-08 13:04:17 +0100

    Ajout de validation sur valeur vide ou null au niveau du DTO

--- demandes_backend/src/demandes/dto/createDemande.dto.ts
+++ demandes_backend/src/demandes/dto/createDemande.dto.ts
@@ -1,15 +1,22 @@
 // import { IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { CreateDateColumn } from 'typeorm';
 
 export class CreateDemandeDto {
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   titre!: string;
 
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   details!: string;
 
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   status!: string;
 
   @ApiProperty()
